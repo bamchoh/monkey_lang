d820733840c07fb9bd100c65c108e5364ca2f498
bamchoh <bamchoh@gmail.com>
2020-02-03 00:39:08 +0900

    Today's progress

--- object/object.go
+++ object/object.go
@@ -6,9 +6,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 // Object is object in this lang
@@ -58,4 +59,16 @@ func (n *Null) Type() ObjectType {
 // Inspect returns null value
 func (n *Null) Inspect() string {
 	return "null"
+}
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
+}
+
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
 }
